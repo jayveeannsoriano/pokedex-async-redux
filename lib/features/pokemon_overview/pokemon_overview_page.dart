c730be03f11b1805b4089695b9c45af978d36015
jayveeannsoriano <2190860@slu.edu.ph>
2023-03-27 18:18:42 +0800

    feat: implemented loading and error state handling - removed _scaffoldKey and utilized ScaffoldMessenger - modified _showErrorMessageSnackbar function

--- lib/features/pokemon_overview/pokemon_overview_page.dart
+++ lib/features/pokemon_overview/pokemon_overview_page.dart
@@ -4,8 +4,6 @@ import 'package:pokedex_async_redux/utils/constants.dart';
 import 'package:pokedex_async_redux/widgets/pokemon_tile_card.dart';
 import 'package:flutter/material.dart';
 
-final _scaffoldKey = GlobalKey<ScaffoldMessengerState>();
-
 class PokemonOverviewPage extends StatelessWidget {
   const PokemonOverviewPage({
     required this.pokemons,
@@ -17,29 +15,33 @@ class PokemonOverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      scaffoldMessengerKey: _scaffoldKey,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(pokemonOverviewTitle)),
-        body: pokemons.when(
-          (data) => GridView.builder(
-            padding: const EdgeInsets.all(10.0),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemCount: data.length,
-            itemBuilder: (_, index) {
-              final pokemon = data[index];
-              return PokemonTileCard(pokemon: pokemon);
+      home: Builder(
+        builder: (BuildContext context) => Scaffold(
+          appBar: AppBar(title: const Text(pokemonOverviewTitle)),
+          body: pokemons.when(
+            (data) => GridView.builder(
+              padding: const EdgeInsets.all(10.0),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemCount: data.length,
+              itemBuilder: (_, index) {
+                final pokemon = data[index];
+                return PokemonTileCard(pokemon: pokemon);
+              },
+            ),
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (errorMessage) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                _showErrorMessageSnackbar(context, errorMessage);
+              });
+              return const Center(child: Text(emptyPokemonsLabel));
             },
           ),
-          loading: () => const Center(child: CircularProgressIndicator()),
-          error: (errorMessage) {
-            SnackBar snackBar = _showErrorMessageSnackbar(errorMessage);
-            _scaffoldKey.currentState?.showSnackBar(snackBar);
-            return const Center(child: Text(emptyPokemonsLabel));
-          },
         ),
       ),
     );
   }
 
-  SnackBar _showErrorMessageSnackbar(String? errorMessage) => SnackBar(content: Text(errorMessage!));
+  _showErrorMessageSnackbar(BuildContext context, String? errorMessage) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(errorMessage!)));
+  }
 }
